e06ab440b94e20f475b68551a83ab8d495e47bc3
Craig <indigomx9@gmail.com>
2021-07-07 19:44:49 -0400

    Initial setup of the Webpack, TypeScript, React app with Testing.

---
+++ README.md
@@ -0,0 +1,5 @@
+# React Testing 
+
+Testing with Webpack, TypeScript, React, Jest, and React Testing Library.
+
+
---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+
+export const App = (): JSX.Element => {
+    return (
+        <React.Fragment>
+            <h1>App</h1>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sequi iure quisquam quis facere quia labore dignissimos, cumque deleniti totam aliquid laborum voluptas sunt maxime itaque harum. Voluptatibus unde asperiores repudiandae.</p>
+        </React.Fragment>
+    );
+};
+
+
+
+
+
+
